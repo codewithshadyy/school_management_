cc08c912e8a7794907c6a4e4949b4329b9cee1ca
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-13 20:25:50 +0300

    created user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("student", "Student"),
+        ("admin", "Admin"),
+        ("tutor", "Tutor"),
+        ("worker", "Worker")
+    )
+    
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES,default="student")
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = "username"
+    USERNAME_FIELD = "email"
+    
+    def __str__(self):
+        return f"{self.email}"
+    
+    
+    
+    
+    
+    
